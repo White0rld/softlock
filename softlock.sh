d46e64d2d91ba50fc9570314ea6edaeebe1f7765
Loic <loic.battesti@epita.fr>
2024-02-18 23:57:48 +0100

    fix: fixed softlock.sh file so that it's more understandable

--- softlock.sh
+++ softlock.sh
@@ -1,5 +1,4 @@
-while [ 1 -eq 1 ];do
-    sleep 1;
-    i3lock;
+while true; do
+    sleep 2;    # Change this value to more or less depending on how hard you want it
+    i3lock -u;  # If they don't have i3lock, change it to whatever they have
 done
-exit 0;
